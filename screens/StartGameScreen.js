4dfa1a4cf1a8ecdf41eb05db6665abeae517fcc4
1997808 <khanhhuyle22@gmail.com>
2020-03-10 11:01:46 +0700

    Card style to component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
+import Card from '../components/Card'
+
 const StartGameScreen = props =>  {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>The Game Screen!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}}/>
           <Button title="Confirm" onPress={() => {}}/>
         </View>
-      </View>
+      </Card>
     </View>
   )
 }
@@ -32,7 +34,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   buttonContainer: {
